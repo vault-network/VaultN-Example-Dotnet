225ef6df994f5795b7794edc975145932fbbdf54
Serhat Yalcin <serhatyalcin@gmail.com>
2020-05-06 17:04:50 +0300

    Controller and Manager implementations

--- vaultn-example-app/vaultn-example-app/Utils/ApiEndPointPrefix.cs
+++ vaultn-example-app/vaultn-example-app/Utils/ApiEndPointPrefix.cs
@@ -2,10 +2,10 @@ namespace vaultn_example_app
 {
     public class ApiEndPointPrefix
     {
-        public const string Ping = "https://onboarding.vaultn.com/api/v1/ping";
-        public const string Product = "https://onboarding.vaultn.com/api/v1/product";
-        public const string ProductType = "https://onboarding.vaultn.com/api/v1/producttype";
-        public const string Transaction = "https://onboarding.vaultn.com/api/v1/transaction";
-        public const string Agreement = "https://onboarding.vaultn.com/api/v1/agreement";
+        public const string Ping = "https://tst-api.vaultn.com/api/v1/ping";
+        public const string Product = "https://tst-api.vaultn.com/api/v1/product";
+        public const string ProductType = "https://tst-api.vaultn.com/api/v1/producttype";
+        public const string Transaction = "https://tst-api.vaultn.com/api/v1/transaction";
+        public const string Agreement = "https://tst-api.vaultn.com/api/v1/agreement";
     }
 }
